bd3da9bfd953d7cb0064a05b9e8635948faf370d
Sumit <sumitkanth999@gmail.com>
2022-08-20 15:39:23 +0530

    added one more solution to seum of series problem

--- array_school/sumOfSeries.cpp
+++ array_school/sumOfSeries.cpp
@@ -2,11 +2,24 @@
 using namespace std;
 
 int seriesSum(int n){
-    return (n * (n+1)) / 2;
+    return (n * (n+1)) / 2; // Time Complexity O(1)
 }
 
 int main(){
-    int n = 10;
+    int n = 5;
     cout << seriesSum(n);
     return 0;
 }
+
+/*
+// Time complexity O(n) bcoz of recursion
+int ans = 0;
+int seriesSum(int n){
+    if(n!=0){
+        ans+= n;
+        seriesSum(n-1);
+    }
+    return ans;
+}
+
+*/
