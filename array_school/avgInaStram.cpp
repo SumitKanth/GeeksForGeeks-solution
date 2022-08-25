7afed807c62fc70ed4cffdda62e452053bf05f75
Sumit <sumitkanth999@gmail.com>
2022-08-25 09:23:22 +0530

    deleted worng code in [Average in a stream

--- array_school/avgInaStram.cpp
+++ array_school/avgInaStram.cpp
@@ -23,18 +23,3 @@ int main(){
     }
     return 0;
 }
-
-/*
-// Another solution
-int maxSubArr(int a[], int n){
-    int max_so_far=a[0];
-    int curr_max=a[0];
-    for(int i=1;i<n;i++)
-    {
-    curr_max=max(a[i],curr_max+a[i]);
-    max_so_far=max(curr_max,max_so_far);
-    }
-    return max_so_far;
-}
-
-*/
